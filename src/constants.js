22c4984a0f3420c2618788503f5e8e46a84aab57
tonimirstoyanov <93668598+tonimirstoyanov@users.noreply.github.com>
2021-12-21 21:25:29 +0200

    changes

--- src/constants.js
+++ src/constants.js
@@ -2,4 +2,6 @@ exports.PORT = process.env.PORT || 3030;
 exports.DB_CONNECTION_URL = 'mongodb://localhost:27017/carDealers'
 exports.SALTROUNDS = 10;
 exports.JWT_SECRET = 'A16B252FB484E25B'
-exports.AUTH_COOKIE_NAME  = 'auth_token';
+exports.AUTH_COOKIE_NAME = 'auth_token';
+exports.CONNECTION_URL = process.env.CONNECTION_URL || 'mongodb+srv://tonimirstoyanov:mhwb3nlYsKef2gP4@car-dealers.i1ff0.mongodb.net/car-dealers?retryWrites=true&w=majority'
+exports.NODE_ENV = 'development'
--- src/server.js
+++ src/server.js
@@ -2,8 +2,11 @@ const express = require('express')
 const cors = require('cors')
 const mongoose = require('mongoose');
 const cookieParser = require('cookie-parser')
+const path = require('path')
 
-const { PORT, DB_CONNECTION_URL } = require('./constants');
+
+
+const { PORT, CONNECTION_URL} = require('./constants');
 const routes = require('./routes');
 
 const app = express();
@@ -11,12 +14,18 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
-// app.get('/', (req, res) => {
-//     res.json({ text: 'its working' })
-// })
 
 app.use(routes)
 
-mongoose.connect(DB_CONNECTION_URL)
+
+app.get('/', (req, res) => {
+    res.send('Api running')
+})
+
+
+mongoose.connect(CONNECTION_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+})
     .then(() => app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`)))
     .catch((err) => console.log('Database connection error:', err.message))
